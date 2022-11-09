3f81db654db8988dbfecc5e54edc535b9d2aabdb
my-rooftop <dgwogh@dgist.ac.kr>
2022-11-10 02:18:40 +0900

    ~6.5

--- main.go
+++ main.go
@@ -1,57 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/nomadcoders/nomadcoin/rest"
 )
 
-const port string = ":4000"
-
-type URL string
-
-func (u URL) MarshalText() ([]byte, error) {
-	url := fmt.Sprintf("http://localhost%s%s", port, u)
-	return []byte(url), nil
-}
-
-type URLDescription struct {
-	URL         URL    `json:"url"`
-	Method      string `json:"method"`
-	Description string `json:"description"`
-	Payload     string `json:"payload,omitempty"`
-}
-
-func (u URLDescription) String() string {
-	return "Hello I'm the URL Description"
-}
-
-func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []URLDescription{
-		{
-			URL:         URL("/"),
-			Method:      "GET",
-			Description: "See Documentation",
-		},
-		{
-			URL:         URL("/blocks"),
-			Method:      "POST",
-			Description: "Add a block",
-			Payload:     "data:string",
-		},
-	}
-	rw.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(rw).Encode(data)
-}
-
 func main() {
-	fmt.Println(URLDescription{
-		URL:         "/",
-		Method:      "GET",
-		Description: "See Documentation",
-	})
-	http.HandleFunc("/", documentation)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	rest.Start(4000)
 }
